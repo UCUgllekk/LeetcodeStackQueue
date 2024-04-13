6596dd27ab96a23f52cafdcc6703e3f654860b33
Roman Pavlosiuk <romanpavlosiuk10@gmail.com>
2024-04-13 17:35:22 +0300

    Implement Stack using Queues completed

---
+++ Implement Stack using Queues/Implement Stack using Queues.py
@@ -0,0 +1,41 @@
+'''Stack implementation'''
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+class MyStack:
+    '''My stack'''
+    def __init__(self):
+        self.head = None
+
+    def push(self, x: int) -> None:
+        '''Adds an element to the top of the stack'''
+        if not self.head:
+            self.head = Node(x)
+        else:
+            old_head = self.head
+            self.head = Node(x)
+            self.head.next = old_head
+
+    def pop(self) -> int:
+        '''Removes the top element from the stack'''
+        data = self.head.data
+        self.head = self.head.next
+        return data
+
+    def top(self) -> int:
+        '''Returns data of head'''
+        return self.head.data
+
+    def empty(self) -> bool:
+        '''Checks if the stack is empty'''
+        return not self.head
+
+if __name__ == '__main__':
+    myStack = MyStack()
+    myStack.push(1)
+    myStack.push(2)
+    assert myStack.top() == 2; # return 2
+    assert myStack.pop() == 2; # return 2
+    assert myStack.empty() is False; # return False
